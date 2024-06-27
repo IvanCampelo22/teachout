d853d0a8d3c86b4c3d83f8482f91736b6410b729
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2024-06-26 23:08:17 -0300

    feat: Add Role

--- user/models.py
+++ user/models.py
@@ -20,3 +20,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+    
+
+# class Role(models.Model):
+
+#     id = models.AutoField(primary_key=True)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.user_id
